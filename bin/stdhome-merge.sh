98446967e3cde20fc7cadaf75e231aeabafccd0b
Jeff Malone <jeff@apr16>
2018-08-19 18:44:19 +0200

    auto commit Sun Aug 19 18:44:19 CEST 2018

---
+++ bin/stdhome-merge.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+DIR="$( cd -P "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd )"
+source $DIR/dot.lockfunctions
+exlock_now || { echo "Couldn't aquire exclusive lock" && exit 1; }
+DIR="$( cd -P "$( dirname $(readlink  -f "${BASH_SOURCE[0]}" ) )/.." && pwd )"
+$DIR/bin/stdhome-remove-deadlinks.sh
+unset GIT_DIR
+unset GIT_WORK_TREE
+
+cd $DIR
+branch=stdhome
+for remote in $(git remote show); do
+	if [[ "$remote" == ksgitlab ]] && [[ -f ~/.ssh/id_rsa_ks ]] && ! ssh-add -L | grep -q id_rsa_ks; then
+		ssh-add ~/.ssh/id_rsa_ks
+	fi
+	git fetch $remote $branch
+done
+git remote show | grep -q . && \
+	git merge -m automerge $(git remote show | sed -r -e "s/$/\\/$branch/")
+set -x
+bash -x $DIR/bin/stdhome-remove-deadlinks.sh
+$DIR/bin/stdothers.sh | while read repo; do
+	git remote show | grep -q . && \
+		git merge -m automerge $(git remote show | sed -r -e "s/$/\\/$branch/")
+	set +x
+done
+exit 0
