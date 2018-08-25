a009271fe8ab1aef63074838b1a8f7ecfd3444c3
Jeff Malone <jeff@apr16>
2018-08-25 10:55:55 +0200

    auto commit Sat Aug 25 10:55:55 CEST 2018

--- bin/stdhome-remove-deadlinks.sh
+++ bin/stdhome-remove-deadlinks.sh
@@ -3,5 +3,5 @@
 set -u
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export STDHOME_DIRNAME="$($DIR/stdhome-dirname.sh)"
-find ~ -type l -lname *${STDHOME_DIRNAME}/* -and -not -path "${STDHOME_DIRNAME}/*" -and -not -exec test -e {} \; -print -delete 2>/dev/null
+find ~ -xdev -type l -lname *${STDHOME_DIRNAME}/* -and -not -path "${STDHOME_DIRNAME}/*" -and -not -exec test -e {} \; -print -delete 2>/dev/null
 exit 0
