0bbedfc316161148ebd8883181783d6bb5a32f17
Jeff Malone <jeff@dec17>
2019-02-27 10:57:35 +0100

    auto commit Wed Feb 27 10:57:35 CET 2019

--- bin/stdhome-pull.sh
+++ bin/stdhome-pull.sh
@@ -10,50 +10,50 @@ unset GIT_DIR
 unset GIT_WORK_TREE
 
 cd $DIR
-[[ ! -d ~/.ssh/c ]] && mkdir ~/.ssh/c
+[[ ! -d ~/.ssh/c ]] && mkdir -p ~/.ssh/c
 [[ -f ~/.ssh/config ]] && chmod g-rwx,o-rwx ~/.ssh/config
 branch=stdhome
 
 f=$DIR/bin/dot.bashfunctions
 if [[ -f $f ]]; then
-	source $f
-   	start_agent_if_not_started
-	mr_ssh_add
+    source $f
+       start_agent_if_not_started
+    mr_ssh_add
 fi
 test -x ~/bin/container-ip.sh && ~/bin/container-ip.sh &>/dev/null
 for remote in $(git remote show); do
-	if [[ "$remote" == ksgitlab ]] && [[ -f ~/.ssh/id_rsa_ks ]] && ! ssh-add -L | grep -q id_rsa_ks; then
-		ssh-add ~/.ssh/id_rsa_ks
-	fi
-	git fetch $remote $branch
+    if [[ "$remote" == ksgitlab ]] && [[ -f ~/.ssh/id_rsa_ks ]] && ! ssh-add -L | grep -q id_rsa_ks; then
+        ssh-add ~/.ssh/id_rsa_ks
+    fi
+    git fetch $remote $branch
 done
 git remote show | grep -q . && \
-	git merge -m automerge $(git remote show | sed -r -e "s/$/\\/$branch/")
+    git merge -m automerge $(git remote show | sed -r -e "s/$/\\/$branch/")
 set -x
 bash -x $DIR/bin/stdhome-remove-deadlinks.sh
 $DIR/bin/stdothers.sh | while read repo; do
-	set -x
-	#export GIT_DIR="$repo/.git"
-	#export GIT_WORK_TREE="$HOME"
-	#git config core.worktree "$GIT_WORK_TREE"
-	cd $repo
-	branch=$(basename $repo noexternalcheckout)
-	test $branch == stdtsys && branch=$(git rev-parse --abbrev-ref HEAD)
-	for remote in $(git remote show); do
-		if [[ "$remote" == ksgitlab ]] && [[ -f ~/.ssh/id_rsa_ks ]] && ! ssh-add -L | grep -q id_rsa_ks; then
-			ssh-add ~/.ssh/id_rsa_ks
-		fi
-		curbranch=$(git rev-parse --abbrev-ref HEAD)
-		if [[ "$branch" != "$curbranch" ]]; then
-			git checkout -b "$branch" &>/dev/null || git checkout "$branch"
-			git branch -D master || true
-		fi
-		#git pull $remote $branch --no-rebase
-		git fetch $remote $branch 
-	done
-	git remote show | grep -q . && \
-		git merge -m automerge $(git remote show | sed -r -e "s/$/\\/$branch/")
-	set +x
+    set -x
+    #export GIT_DIR="$repo/.git"
+    #export GIT_WORK_TREE="$HOME"
+    #git config core.worktree "$GIT_WORK_TREE"
+    cd $repo
+    branch=$(basename $repo noexternalcheckout)
+    test $branch == stdtsys && branch=$(git rev-parse --abbrev-ref HEAD)
+    for remote in $(git remote show); do
+        if [[ "$remote" == ksgitlab ]] && [[ -f ~/.ssh/id_rsa_ks ]] && ! ssh-add -L | grep -q id_rsa_ks; then
+            ssh-add ~/.ssh/id_rsa_ks
+        fi
+        curbranch=$(git rev-parse --abbrev-ref HEAD)
+        if [[ "$branch" != "$curbranch" ]]; then
+            git checkout -b "$branch" &>/dev/null || git checkout "$branch"
+            git branch -D master || true
+        fi
+        #git pull $remote $branch --no-rebase
+        git fetch $remote $branch
+    done
+    git remote show | grep -q . && \
+        git merge -m automerge $(git remote show | sed -r -e "s/$/\\/$branch/")
+    set +x
 done
 [[ -d ~/.tmp/touch ]] && touch ~/.tmp/touch/stdhome-pull
 exit 0
