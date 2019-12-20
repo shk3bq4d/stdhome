986bc72e1bc1f91fa74a0d52ab78181925c75447
Jeff Malone <jeff@dec17>
2019-12-20 20:05:08 +0100

    auto commit Fri Dec 20 20:05:08 CET 2019

--- bin/stdhome-push.sh
+++ bin/stdhome-push.sh
@@ -30,7 +30,7 @@ $DIR/bin/stdothers.sh | grep -vE 'stdks' | while read repo; do
     test $branch == stdtsys && branch=$(git rev-parse --abbrev-ref HEAD)
     if [[ "$repo" != *noexternalcheckout ]]; then
         #find "$repo" -xdev -mindepth 1 -not -path '*/.git*' -print -delete
-        find "$repo" -xdev -mindepth 1 \( -not -path '*/.git*' -and -not -path '*/secrets*' \) -print# -delete
+        find "$repo" -xdev -mindepth 1 \( -not -path '*/.git*' -and -not -path '*/secrets*' \) -print -delete
     fi
     for remote in $(git remote show); do
         #if [[ "$remote" == ksgitlab ]] && [[ -f ~/.ssh/id_rsa_ks ]] && ! ssh-add -L | grep -q id_rsa_ks; then
